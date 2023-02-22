653f39b724d4ea5640d7fc41ca6b1c4f52cb7d92
Claric1 <simutundaclaric2@gmail.com>
2023-02-22 13:39:21 -0800

    putchar.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A program that prints _putchar using putchar function
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	putchar('_Putchar');
+	putchar('\n');
+	return (0);
+}
