07b358ede62fb49b82f7abdc1fd95b49065bacdf
Claric1 <simutundaclaric2@gmail.com>
2023-02-22 12:36:45 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -A program that prints the size of various types of bytes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte\n");
+	printf("Size of an int: 4 bytes\n");
+	printf("Size of a long int: 8 bytes\n");
+	printf("Size of a long long int: 8 bytes\n");
+	printf("Size of a float: 4 bytes\n");
+	return (0);
+}
