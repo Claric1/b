871927d9a0dbe3fffe5c20d00ee3b3f73bf8789f
Claric1 <simutundaclaric2@gmail.com>
2023-02-22 08:15:43 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Print a code using puts
+ *
+ * return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"\n);
+	return (0);
+}
