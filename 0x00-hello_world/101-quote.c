2e766829bc617f3b03ca115e7a0bb281d2f63ae6
Claric1 <simutundaclaric2@gmail.com>
2023-02-25 14:16:59 -0800

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -A program that print a line with fput
+ * main -A program that print a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
