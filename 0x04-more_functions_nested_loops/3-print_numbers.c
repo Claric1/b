73d3e44ecba63d7ab25563b3caecc1b744176031
Claric1 <simutundaclaric2@gmail.com>
2023-02-24 18:26:11 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers -function that prints number from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+	{
+	i = 0;
+	for (i >= 0; i <= 9)
+
+	_putchar(i);
+
+	_putchar('\n');
+	}
+}
