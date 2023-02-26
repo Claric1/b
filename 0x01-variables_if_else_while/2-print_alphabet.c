ff25edb15363738bde469ab893f6f5fdd377a30f
Claric1 <simutundaclaric2@gmail.com>
2023-02-26 15:46:41 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -print the alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
