96e635ccd8a9d9f930ee63281ae14bc8129236f7
Claric1 <simutundaclaric2@gmail.com>
2023-02-26 12:33:05 -0800

    positive or negagitive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,15 @@ int main(void)
 
 	if (n == 0)
 	{
-		printf("is zero: %d\n", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("is negative: %d\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("is positive: %d\n", n);
+		printf("%d is positive\n", n);
 	}
-
 	return (0);
 }
