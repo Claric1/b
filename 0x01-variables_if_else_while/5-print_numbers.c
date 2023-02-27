2a6a711141e25c58aaabee0d5fe0bf2f65b09d4b
Claric1 <simutundaclaric2@gmail.com>
2023-02-26 17:22:53 -0800

    single numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -print single numbers of base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
